d23e70de8c153fccde70f44a05e6ada1be20429d
Yuchuan Zhou <MiniHero@Yuchuans-MacBook-Pro.local>
2015-03-23 22:44:27 -0400

    initial commit

---
+++ xmlrpc.php
@@ -0,0 +1,5 @@
+<br />
+<b>Warning</b>:  ini_set() has been disabled for security reasons in <b>/home/antranet/public_html/wp-includes/load.php</b> on line <b>287</b><br />
+<br />
+<b>Warning</b>:  Cannot modify header information - headers already sent by (output started at /home/antranet/public_html/wp-includes/load.php:287) in <b>/home/antranet/public_html/wp-includes/class-IXR.php</b> on line <b>398</b><br />
+XML-RPC server accepts POST requests only.
